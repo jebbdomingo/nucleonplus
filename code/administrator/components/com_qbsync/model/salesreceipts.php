ecaf0342b1ad78421dea7aa291911761cf54c1e0
Jebb Domingo <jebb@wizmediateam.com>
2016-03-22 10:54:57 +0800

    Re: #4 Created sales receipt views
    - Views
    - listbox helper

---
+++ code/administrator/components/com_qbsync/model/salesreceipts.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Nucleon Plus
+ *
+ * @package     Nucleon Plus
+ * @copyright   Copyright (C) 2015 - 2020 Nucleon Plus Co. (http://www.nucleonplus.com)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        https://github.com/jebbdomingo/nucleonplus for the canonical source repository
+ */
+class ComQbsyncModelSalesreceipts extends KModelDatabase
+{
+    public function __construct(KObjectConfig $config)
+    {
+        parent::__construct($config);
+
+        $this->getState()
+            ->insert('synced', 'string')
+        ;
+    }
+
+    protected function _initialize(KObjectConfig $config)
+    {
+        $config->append(array(
+            'behaviors' => array(
+                'searchable' => array('columns' => array('DocNumber'))
+            )
+        ));
+
+        parent::_initialize($config);
+    }
+
+    protected function _buildQueryWhere(KDatabaseQueryInterface $query)
+    {
+        parent::_buildQueryWhere($query);
+
+        $state = $this->getState();
+
+        if ($state->synced && $state->synced <> 'all')
+        {
+            switch ($state->synced) {
+                case 'yes':
+                    $synced = 1;
+                    break;
+
+                case 'no':
+                    $synced = 0;
+                
+                default:
+                    $synced = 0;
+                    break;
+            }
+        
+            $query->where('tbl.synced = :synced')->bind(['synced' => $synced]);
+        }
+    }
+}
