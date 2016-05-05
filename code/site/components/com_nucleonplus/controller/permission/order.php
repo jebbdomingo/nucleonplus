2ae2fbef852b612493f17246e7f5517d0e99a228
Jebb Domingo <jebb@wizmediateam.com>
2016-05-05 17:14:57 +0800

    Fixed typo

--- code/site/components/com_nucleonplus/controller/permission/order.php
+++ code/site/components/com_nucleonplus/controller/permission/order.php
@@ -29,7 +29,7 @@ class ComNucleonplusControllerPermissionOrder extends ComKoowaControllerPermissi
         {
             return false;
         }
-        else return parent:canSave();
+        else return parent::canSave();
     }
 
     /**
