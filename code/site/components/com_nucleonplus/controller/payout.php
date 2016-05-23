3dd44aae07fcaec045946b0018463e3083a0b79f
Jebb Domingo <jebb@wizmediateam.com>
2016-05-23 11:24:54 +0800

    Fixed error in discrepancy checking

--- code/site/components/com_nucleonplus/controller/payout.php
+++ code/site/components/com_nucleonplus/controller/payout.php
@@ -104,7 +104,7 @@ class ComNucleonplusControllerPayout extends ComKoowaControllerModel
 
                 $rewardFrom = $this->getObject('com://admin/nucleonplus.model.rewards')
                     ->id($rebate->reward_id_from)
-                    ->status('active')
+                    ->status('ready')
                     ->fetch()
                 ;
 
