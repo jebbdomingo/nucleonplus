536c291af7a7b4aa1a3be8b7bf6b75d435051259
Jebb Domingo <jebb@timble.net>
2017-01-03 09:19:54 +0800

    Create social meta tags

--- code/plugins/system/nucleonplus/nucleonplus.php
+++ code/plugins/system/nucleonplus/nucleonplus.php
@@ -12,18 +12,60 @@ class PlgSystemNucleonplus extends JPlugin
 {
     public function onAfterDispatch()
     {
+        $request = $this->getObject('request');
+        $input   = JFactory::getApplication()->input;
+
+        // Remember sponsor id from referral link
         $this->_persistSponsorId();
+
+        // Social meta tags for product page
+        $view = $input->get('view', null);
+        $id   = $input->get('id', null);
+
+        if ($view == 'product' && $id) {
+            $this->_generateSocialMetatags($id);
+        }
     }
 
+    /**
+     * Persist sponsor id
+     * 
+     * @return void
+     */
     protected function _persistSponsorId()
     {
-        $request    = $this->getObject('request')->getQuery();
         $input      = JFactory::getApplication()->input;
         $sponsor_id = $input->get('sponsor_id', null);
 
         if ($sponsor_id) {
             $this->getObject('user')->set('sponsor_id', $sponsor_id);
         }
+    }
+
+    /**
+     * Generate social meta tags from item data
+     * 
+     * @param  integer $product_id
+     * 
+     * @return void
+     */
+    protected function _generateSocialMetatags($product_id)
+    {
+        $item = $this->getObject('com://admin/qbsync.model.items')->id($product_id)->fetch();
+
+        $image = JURI::base() . "images/{$item->image}";
+
+        $document = JFactory::getDocument();
+        $document->setMetaData('twitter:card', 'summary');
+        $document->setMetaData('twitter:title', $item->Name);
+        $document->setMetaData('twitter:description', $item->Description);
+        $document->setMetaData('twitter:image:alt', $item->Name);
+        $document->setMetaData('twitter:image', $image);
+        $document->setMetaData('og:type', 'item');
+        $document->setMetaData('og:title', $item->Name);
+        $document->setMetaData('og:description', $item->Description);
+        $document->setMetaData('og:url', JURI::base());
+        $document->setMetaData('og:image', $image);
     }
 
     /**
