f47ed68c940e7a25c5183be3e22af97fa656f11c
Jebb Domingo <jebb@timble.net>
2017-01-05 18:46:56 +0800

    Debug

--- code/administrator/components/com_nucleonplus/controller/cart.php
+++ code/administrator/components/com_nucleonplus/controller/cart.php
@@ -59,12 +59,12 @@ class ComNucleonplusControllerCart extends ComCartControllerCart
     protected function _validateAdd(KControllerContextInterface $context)
     {
         $data           = $context->request->data;
-        $data->row      = $data->ItemRef;
-        $data->quantity = $data->form_quantity;
-
         var_dump($data->toArray());
         echo '<br />';
         die('test');
+        $data->row      = $data->ItemRef;
+        $data->quantity = $data->form_quantity;
+
 
         parent::_validateAdd($context);
     }
