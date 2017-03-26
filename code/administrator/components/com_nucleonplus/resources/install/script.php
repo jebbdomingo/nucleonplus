a70e6517f77a69545b2e83c691dd071900000e75
Jebb Domingo <jebb@timble.net>
2017-03-26 18:29:38 +0800

    Setup installer

--- code/administrator/components/com_nucleonplus/resources/install/script.php
+++ code/administrator/components/com_nucleonplus/resources/install/script.php
@@ -7,20 +7,6 @@
  * @link        https://github.com/jebbdomingo/nucleonplus for the canonical source repository
  */
 
-class com_nucleonplusInstallerScript
+class com_nucleonplusInstallerScript extends JoomlatoolsInstallerHelper
 {
-    public function preflight($type, $installer)
-    {
-        $return = true;
-        
-        if (!class_exists('Koowa'))
-        {
-            $error = 'This component requires Nooku Framework';
-            $installer->getParent()->abort($error);
-
-            $return = false;
-        }
-        
-        return $return;
-    }
 }
